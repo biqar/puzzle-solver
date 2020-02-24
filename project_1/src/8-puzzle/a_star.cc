25941977eaed180d117bd91f49196bccaf2ba29e
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2020-02-24 15:20:14 -0500

    Update a_star implementation for 8 puzzle

--- project_1/src/8-puzzle/a_star.cc
+++ project_1/src/8-puzzle/a_star.cc
@@ -9,6 +9,7 @@
 
 #include <stdio.h>
 #include <queue>
+#include <map>
 #include <list>
 
 class AStarEightPuzzle : public Solver {
@@ -28,6 +29,10 @@ private:
     State *goal_state;
     State *initial_state;
     Heuristic *heuristic;
+    std::priority_queue<Node *, vector<Node *>, NodeComparatorOnTotalCost> q;
+    std::map<long long int, bool> m;        //mapping already explored nodes
+    int node_expanded;
+    int node_generated;
 
     void run_astar();
 };
@@ -36,38 +41,52 @@ int AStarEightPuzzle::init(State *_initial_state, State *_goal_state, Heuristic 
     goal_state = _goal_state;
     initial_state = _initial_state;
     heuristic = _heuristic;
+    node_expanded = 0;
+    node_generated = 1;
+    q.push(create_new_node(0, heuristic->guess_distance(initial_state, goal_state), NULL, initial_state));
+
     return 1;
 }
 
 void AStarEightPuzzle::run_astar() {
-    int node_expanded = 0;
-    std::queue<Node *> q;
-    q.push(create_new_node(0, heuristic->guess_distance(initial_state, goal_state), NULL, initial_state));
-
-    while(!q.empty()) {
-        Node *current_node = q.front(); q.pop();
-
-        printf("current node depth: %d\n", current_node->depth);
-        print_board(current_node->state);
-
-        if(equal_state(current_node->state, goal_state)) {
-            printf("solution found!");
-            break;
-        }
-        std::list<Node *> child_list = expand_node(current_node, goal_state, heuristic);
-        node_expanded += 1;
-        child_list.sort(NodeComparatorOnTotalCost());
-        for (std::list<Node *>::iterator it=child_list.begin(); it != child_list.end(); ++it) {
-            q.push(*it);
-        }
-
-        //if(node_expand > 20) break;
+    if(equal_state(initial_state, goal_state)) {
+        printf("solution found!\n");
+        print_board(initial_state);
+        return;
     }
-    printf("found solution by expending [%d] nodes\n", node_expanded);
+    while(!q.empty()) {
+        Node *current_node = q.top(); q.pop();
+
+//        printf("current node depth: %d\n", current_node->depth);
+//        print_board(current_node->state);
+
+        long long int current_node_key = construct_board_key(current_node->state);
+        if(m.find(current_node_key) != m.end()) {
+            continue;
+        }
+        node_expanded += 1;
+
+        std::list<Node *> child_list = expand_node(current_node, goal_state, heuristic);
+        for (std::list<Node *>::iterator it=child_list.begin(); it != child_list.end(); ++it) {
+            if(equal_state((*it)->state, goal_state)) {
+                printf("solution found!\n");
+                print_path(*it);
+                return;
+            }
+
+            long long int child_node_key = construct_board_key((*it)->state);
+            if(m.find(child_node_key) == m.end()) {
+                q.push(*it);
+                node_generated += 1;
+            }
+        }
+        m[current_node_key] = true;
+    }
 }
 
 int AStarEightPuzzle::run() {
     run_astar();
+    printf("[8-puzzle] [A-Star] generated_nodes: [%d], expanded_node: [%d]\n", node_generated, node_expanded);
     return 1;
 }
 
