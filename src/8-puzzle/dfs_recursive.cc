65276a626f4da27133f04c59a80f812e6c9a9101
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2020-02-24 18:24:27 -0500

    Refactor dfs solutions for 8-puzzle

--- src/8-puzzle/dfs_recursive.cc
+++ src/8-puzzle/dfs_recursive.cc
@@ -32,6 +32,7 @@ private:
 
     std::map<long long int, bool> m;
     int node_expanded;
+    int node_generated;
 
     bool run_dfs_recursive(Node *current_node);
 };
@@ -43,25 +44,21 @@ int DfsRecursivEightPuzzle::init(State *_initial_state, State *_goal_state, Heur
     is_print_path = _print_path;
 
     node_expanded = 0;
+    node_generated = 1;
     m[construct_board_key(initial_state)] = true;
 
     return 1;
 }
 
 bool DfsRecursivEightPuzzle::run_dfs_recursive(Node *current_node) {
-    //printf("current node depth: %d\n", current_node->depth);
-    //printf("board_key: %lld\n", construct_board_key(current_node->state));
-    //print_board(current_node->state);
-
-    //if(node_expanded > 400) return;
-
     if(equal_state(current_node->state, goal_state)) {
-        printf("found solution by expending [%d] nodes\n", node_expanded);
+        printf("solution found!\n");
         return true;
     }
 
     std::list<Node *> child_list = expand_node(current_node, goal_state, heuristic);
     node_expanded += 1;
+    node_generated += child_list.size();
     for (std::list<Node *>::iterator it=child_list.begin(); it != child_list.end(); ++it) {
         long long int child_state_key = construct_board_key((*it)->state);
         if(m.find(child_state_key) == m.end()) {
@@ -75,6 +72,7 @@ bool DfsRecursivEightPuzzle::run_dfs_recursive(Node *current_node) {
 
 int DfsRecursivEightPuzzle::run() {
     run_dfs_recursive(create_new_node(0, heuristic->guess_distance(initial_state, goal_state), NULL, initial_state));
+    printf("[8-puzzle] [dfs-recursive] generated_nodes: [%d], expanded_node: [%d]\n", node_generated, node_expanded);
     return 1;
 }
 
--- src/8-puzzle/dfs_stack.cc
+++ src/8-puzzle/dfs_stack.cc
@@ -31,7 +31,9 @@ private:
     bool is_print_path = false;
 
     std::map<long long int, bool> m;
+    std::stack<Node *> s;
     int node_expanded;
+    int node_generated;
 
     void run_dfs_stack();
 };
@@ -42,34 +44,38 @@ int DfsStackEightPuzzle::init(State *_initial_state, State *_goal_state, Heurist
     heuristic = _heuristic;
     is_print_path = _print_path;
 
+    s.push(create_new_node(0, heuristic->guess_distance(initial_state, goal_state), NULL, initial_state));
+    m[construct_board_key(initial_state)] = true;
+
     node_expanded = 0;
+    node_generated = 0;
 
     return 1;
 }
 
 void DfsStackEightPuzzle::run_dfs_stack() {
-    std::stack<Node *> s;
-    s.push(create_new_node(0, heuristic->guess_distance(initial_state, goal_state), NULL, initial_state));
-    m[construct_board_key(initial_state)] = true;
+    if(equal_state(initial_state, goal_state)) {
+        printf("solution found!\n");
+        if(is_print_path) print_board(initial_state);
+        return;
+    }
 
     while(!s.empty()) {
         Node *current_node = s.top(); s.pop();
-
-        //printf("current node depth: %d\n", current_node->depth);
-        //printf("board_key: %lld\n", construct_board_key(current_node->state));
-        //print_board(current_node->state);
-
-        //if(node_expanded > 400) return;
-
-        if(equal_state(current_node->state, goal_state)) {
-            printf("found solution by expending [%d] nodes\n", node_expanded);
-            break;
-        }
-        std::list<Node *> child_list = expand_node(current_node, goal_state, heuristic);
         node_expanded += 1;
+
+//        printf("current node depth: %d\n", current_node->depth);
+//        print_board(current_node->state);
+
+        std::list<Node *> child_list = expand_node(current_node, goal_state, heuristic);
         for (std::list<Node *>::iterator it=child_list.begin(); it != child_list.end(); ++it) {
             long long int child_state_key = construct_board_key((*it)->state);
+            if(equal_state((*it)->state, goal_state)) {
+                printf("solution found!\n");
+                return;
+            }
             if(m.find(child_state_key) == m.end()) {
+                node_generated += 1;
                 m[child_state_key] = true;
                 s.push(*it);
             }
@@ -79,6 +85,7 @@ void DfsStackEightPuzzle::run_dfs_stack() {
 
 int DfsStackEightPuzzle::run() {
     run_dfs_stack();
+    printf("[8-puzzle] [dfs-stack] generated_nodes: [%d], expanded_node: [%d]\n", node_generated, node_expanded);
     return 1;
 }
 
