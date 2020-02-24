e684dfd6b2f0702b19744dcad88aa86b5afda9ad
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2020-02-24 18:06:02 -0500

    Refactor bfs bidirectional implementation for 8-puzzle
    Added counter for node generated, applied same optimization strategy used in bfs-opt.

--- src/8-puzzle/bfs_bidir.cc
+++ src/8-puzzle/bfs_bidir.cc
@@ -34,10 +34,11 @@ private:
 
     std::queue<Node *> initial_to_goal_q;
     std::queue<Node *> goal_to_initial_q;
-    std::map<long long int, int> initial_to_goal_m;
-    std::map<long long int, int> goal_to_initial_m;
+    std::map<long long int, bool> initial_to_goal_m;
+    std::map<long long int, bool> goal_to_initial_m;
     bool solution_found;
     int node_expanded;
+    int node_generated;
 
     void initial_to_goal_bfs(int depth);
     void goal_to_initial_bfs(int depth);
@@ -50,18 +51,18 @@ int BfsBidirEightPuzzle::init(State *_initial_state, State *_goal_state, Heurist
     is_print_path = _print_path;
 
     Node *initial_node = create_new_node(0, heuristic->guess_distance(initial_state, goal_state), NULL, initial_state);
-    Node *goal_node = create_new_node(0, heuristic->guess_distance(goal_state, initial_state), NULL, goal_state);
-
     initial_to_goal_q.push(initial_node);
     long long int initial_state_key = construct_board_key(initial_state);
-    initial_to_goal_m[initial_state_key] = 0;
+    initial_to_goal_m[initial_state_key] = true;
 
+    Node *goal_node = create_new_node(0, heuristic->guess_distance(goal_state, initial_state), NULL, goal_state);
     goal_to_initial_q.push(goal_node);
     long long int goal_state_key = construct_board_key(goal_state);
-    goal_to_initial_m[goal_state_key] = 0;
+    goal_to_initial_m[goal_state_key] = true;
 
     solution_found = false;
     node_expanded = 0;
+    node_generated = 2;
 
     return 1;
 }
@@ -74,25 +75,23 @@ void BfsBidirEightPuzzle::goal_to_initial_bfs(int depth) {
         }
 
         goal_to_initial_q.pop();
+        node_expanded += 1;
+
 //        printf("current node depth: %d\n", current_node->depth);
 //        print_board(current_node->state);
 
-        if(solution_found) {
-            printf("[%s] found solution by expending [%d] nodes\n", __func__, node_expanded);
-            return;
-        }
-
-        long long int current_state_key = construct_board_key(current_node->state);
-        if(initial_to_goal_m.find(current_state_key) != initial_to_goal_m.end()) {
-            printf("[%s] found solution by expending [%d] nodes\n", __func__, node_expanded);
-            solution_found = true;
-            return;
-        }
         std::list<Node *> child_list = expand_node(current_node, goal_state, heuristic);
-        node_expanded += 1;
         for (std::list<Node *>::iterator it=child_list.begin(); it != child_list.end(); ++it) {
             long long int child_state_key = construct_board_key((*it)->state);
+            if(initial_to_goal_m.find(child_state_key) != initial_to_goal_m.end()) {
+                printf("[%s] solution found!\n", __func__);
+                solution_found = true;
+                return;
+            }
+
             if(goal_to_initial_m.find(child_state_key) == goal_to_initial_m.end()) {
+                goal_to_initial_m[child_state_key] = true;
+                node_generated += 1;
                 goal_to_initial_q.push(*it);
             }
         }
@@ -107,25 +106,23 @@ void BfsBidirEightPuzzle::initial_to_goal_bfs(int depth) {
         }
 
         initial_to_goal_q.pop();
+        node_expanded += 1;
+
 //        printf("current node depth: %d\n", current_node->depth);
 //        print_board(current_node->state);
 
-        if(solution_found) {
-            printf("[%s] found solution by expending [%d] nodes\n", __func__, node_expanded);
-            return;
-        }
-
-        long long int current_state_key = construct_board_key(current_node->state);
-        if(goal_to_initial_m.find(current_state_key) != goal_to_initial_m.end()) {
-            printf("[%s] found solution by expending [%d] nodes\n", __func__, node_expanded);
-            solution_found = true;
-            return;
-        }
         std::list<Node *> child_list = expand_node(current_node, goal_state, heuristic);
-        node_expanded += 1;
         for (std::list<Node *>::iterator it=child_list.begin(); it != child_list.end(); ++it) {
             long long int child_state_key = construct_board_key((*it)->state);
+            if(goal_to_initial_m.find(child_state_key) != goal_to_initial_m.end()) {
+                printf("[%s] solution found!\n", __func__);
+                solution_found = true;
+                return;
+            }
+
             if(initial_to_goal_m.find(child_state_key) == initial_to_goal_m.end()) {
+                initial_to_goal_m[child_state_key] = true;
+                node_generated += 1;
                 initial_to_goal_q.push(*it);
             }
         }
@@ -133,7 +130,15 @@ void BfsBidirEightPuzzle::initial_to_goal_bfs(int depth) {
 }
 
 int BfsBidirEightPuzzle::run() {
+    if(equal_state(initial_state, goal_state)) {
+        printf("solution found!\n");
+        if(is_print_path) print_board(initial_state);
+        printf("[8-puzzle] [bfs-bidir] generated_nodes: [%d], expanded_node: [%d]\n", node_generated, node_expanded);
+        return 1;
+    }
+
     initial_to_goal_bfs(DEPTH_ONE);
+    printf("[8-puzzle] [bfs-bidir] generated_nodes: [%d], expanded_node: [%d]\n", node_generated, node_expanded);
     return 1;
 }
 
