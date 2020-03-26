9451ab6dd5db8348025b8c29c8c9e068682b56f5
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2020-03-25 20:08:52 -0400

    Add benchmark script for basic hill-climbing algorithm

---
+++ benchmark/n-queen/hill_climbing-benchmark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# use for loop to run each case for 500 times
+counter=1
+while [ $counter -le 500 ]
+do
+  ./../../puzzle -problem n-queen -algo hill-climbing -mode na -board_dim 10 -mx_sideways_move 0 -print_path false
+  ((counter++))
+done
