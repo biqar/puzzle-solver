598cca5eb7470e40475826c0af4a0fec1a2db923
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2020-04-23 15:38:12 -0400

    Refactor solver factory code

--- src/core/solver_factory.cc
+++ src/core/solver_factory.cc
@@ -31,67 +31,124 @@ using utils::Properties;
 Solver *SolverFactory::CreateSolver(utils::Properties &props, Heuristic *_heuristic) {
     if (props["puzzle"] == "8-puzzle") {
         if(props["algorithm"] == "a_star") {
-            return new AStarEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+            return new AStarEightPuzzle(new EightPuzzleInitParam(
+                    construct_state(props["initial_state"]),
+                    construct_state(props["goal_state"]),
+                    _heuristic, utils::to_bool(props["print_path"])));
         }
         if(props["algorithm"] == "ida_star") {
-            return new IdaStarEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+            return new IdaStarEightPuzzle(new EightPuzzleInitParam(
+                    construct_state(props["initial_state"]),
+                    construct_state(props["goal_state"]),
+                    _heuristic, utils::to_bool(props["print_path"])));
         }
         if(props["algorithm"] == "bfs") {
             if(props["mode"] == "bi-directional") {
-                return new BfsBidirEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+                return new BfsBidirEightPuzzle(new EightPuzzleInitParam(
+                        construct_state(props["initial_state"]),
+                        construct_state(props["goal_state"]),
+                        _heuristic, utils::to_bool(props["print_path"])));
             }
             if(props["mode"] == "greedy") {
-                return new BfsGreedyEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+                return new BfsGreedyEightPuzzle(new EightPuzzleInitParam(
+                        construct_state(props["initial_state"]),
+                        construct_state(props["goal_state"]),
+                        _heuristic, utils::to_bool(props["print_path"])));
             }
             if(props["mode"] == "optimized") {
-                return new BfsOptEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+                return new BfsOptEightPuzzle(new EightPuzzleInitParam(
+                        construct_state(props["initial_state"]),
+                        construct_state(props["goal_state"]),
+                        _heuristic, utils::to_bool(props["print_path"])));
             }
-            return new BfsEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+            return new BfsEightPuzzle(new EightPuzzleInitParam(
+                    construct_state(props["initial_state"]),
+                    construct_state(props["goal_state"]),
+                    _heuristic, utils::to_bool(props["print_path"])));
         }
         if(props["algorithm"] == "dfs") {
             if(props["mode"] == "recursive") {
-                return new DfsRecursivEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+                return new DfsRecursivEightPuzzle(new EightPuzzleInitParam(
+                        construct_state(props["initial_state"]),
+                        construct_state(props["goal_state"]),
+                        _heuristic, utils::to_bool(props["print_path"])));
             }
             if(props["mode"] == "stack-based") {
-                return new DfsStackEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+                return new DfsStackEightPuzzle(new EightPuzzleInitParam(
+                        construct_state(props["initial_state"]),
+                        construct_state(props["goal_state"]),
+                        _heuristic, utils::to_bool(props["print_path"])));
             }
             return NULL;
         }
         if(props["algorithm"] == "dls") {
             if(props["mode"] == "bi-directional") {
-                return new DlsBidirEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+                return new DlsBidirEightPuzzle(new EightPuzzleInitParam(
+                        construct_state(props["initial_state"]),
+                        construct_state(props["goal_state"]),
+                        _heuristic, utils::to_bool(props["print_path"])));
             }
-            return new DlsEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+            return new DlsEightPuzzle(new EightPuzzleInitParam(
+                    construct_state(props["initial_state"]),
+                    construct_state(props["goal_state"]),
+                    _heuristic, utils::to_bool(props["print_path"])));
         }
         if(props["algorithm"] == "ids") {
             if(props["mode"] == "bi-directional") {
-                return new IdsBidirEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+                return new IdsBidirEightPuzzle(new EightPuzzleInitParam(
+                        construct_state(props["initial_state"]),
+                        construct_state(props["goal_state"]),
+                        _heuristic, utils::to_bool(props["print_path"])));
             }
-            return new IdsEightPuzzle(new EightPuzzleInitParam(construct_state(props["initial_state"]), construct_state(props["goal_state"]), _heuristic, utils::to_bool(props["print_path"])));
+            return new IdsEightPuzzle(new EightPuzzleInitParam(
+                    construct_state(props["initial_state"]),
+                    construct_state(props["goal_state"]),
+                    _heuristic, utils::to_bool(props["print_path"])));
         }
         return NULL;
     }
     else if (props["puzzle"] == "n-queen") {
         if(props["algorithm"] == "hill-climbing") {
             if(props["mode"] == "random-best") {
-                return new HillClimbingRandBestNQueens(new NQueenInitParam(std::stoi(props["dim"]), std::stoi(props["mx_sways_move"]), utils::to_bool(props["print_path"])));
+                return new HillClimbingRandBestNQueens(new NQueenInitParam(
+                        std::stoi(props["dim"]),
+                        std::stoi(props["mx_sways_move"]),
+                        utils::to_bool(props["print_path"])));
             }
             if(props["mode"] == "first-choice") {
-                return new HillClimbingFCNQueens(new NQueenInitParam(std::stoi(props["dim"]), std::stoi(props["mx_sways_move"]), utils::to_bool(props["print_path"])));
+                return new HillClimbingFCNQueens(new NQueenInitParam(
+                        std::stoi(props["dim"]),
+                        std::stoi(props["mx_sways_move"]),
+                        utils::to_bool(props["print_path"])));
             }
             if(props["mode"] == "stochastic") {
-                return new HillClimbingStochasticNQueens(new NQueenInitParam(std::stoi(props["dim"]), std::stoi(props["mx_sways_move"]), utils::to_bool(props["print_path"])));
+                return new HillClimbingStochasticNQueens(new NQueenInitParam(
+                        std::stoi(props["dim"]),
+                        std::stoi(props["mx_sways_move"]),
+                        utils::to_bool(props["print_path"])));
             }
             if(props["mode"] == "sideways-move") {
-                return new HillClimbingWSideWaysNQueens(new NQueenInitParam(std::stoi(props["dim"]), std::stoi(props["mx_sways_move"]), utils::to_bool(props["print_path"])));
+                return new HillClimbingWSideWaysNQueens(new NQueenInitParam(
+                        std::stoi(props["dim"]),
+                        std::stoi(props["mx_sways_move"]),
+                        utils::to_bool(props["print_path"])));
             }
             if(props["mode"] == "random-restart") {
-                return new HillClimbingRRNQueens(new NQueenInitParam(std::stoi(props["dim"]), std::stoi(props["mx_sways_move"]), utils::to_bool(props["print_path"])));
+                return new HillClimbingRRNQueens(new NQueenInitParam(
+                        std::stoi(props["dim"]),
+                        std::stoi(props["mx_sways_move"]),
+                        utils::to_bool(props["print_path"])));
             }
             if(props["mode"] == "random-restart-sways-move") {
-                return new HillClimbingRRWSideWaysNQueens(new NQueenInitParam(std::stoi(props["dim"]), std::stoi(props["mx_sways_move"]), utils::to_bool(props["print_path"])));
+                return new HillClimbingRRWSideWaysNQueens(new NQueenInitParam(
+                        std::stoi(props["dim"]),
+                        std::stoi(props["mx_sways_move"]),
+                        utils::to_bool(props["print_path"])));
             }
-            return new HillClimbingNQueens(new NQueenInitParam(std::stoi(props["dim"]), std::stoi(props["mx_sways_move"]), utils::to_bool(props["print_path"])));
+            return new HillClimbingNQueens(new NQueenInitParam(
+                    std::stoi(props["dim"]),
+                    std::stoi(props["mx_sways_move"]),
+                    utils::to_bool(props["print_path"])));
         }
         return NULL;
     }
